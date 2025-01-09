d2d7ad9d50cd2b8f8b60011c3d53ec21ca76d126
gouhongshen <gouhongshen@hotmail.com>
2025-01-09 18:55:22 +0800

    update

--- checkunsafe/checkunsafe.go
+++ checkunsafe/checkunsafe.go
@@ -96,6 +96,7 @@ var importWhiteList = []string{
 	"github.com/matrixorigin/matrixone/pkg/util/trace/impl/motrace/report_statement.go",
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/disttae/cache/types.go",
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/disttae/logtailreplay/partition_state.go",
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/disttae/logtailreplay/types.go",
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/disttae/txn_table.go",
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/disttae/mo_table_stats.go",
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog/basemvccnode.go",
